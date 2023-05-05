5de4a84474c0fc68b8e669faefed4f67a36d437d
SergioFAvilaR <132033828+SergioFAvilaR@users.noreply.github.com>
2023-05-04 22:08:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Ya puedes descargar nuestra app en iOS y Android!')
+  .pauseFor(2000)
+  .deleteChars(10)
+  .start();
+
+  
